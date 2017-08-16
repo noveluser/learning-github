dd26bb77cf6a568a7c8094eaf0feba50980b3e67
wangle <flywangle@hotmail.com>
2017-08-16 11:43:00 +0800

    add a time stamp

--- restart.py
+++ restart.py
@@ -1,30 +1,29 @@
 #coding=utf-8
 import requests
 from requests.exceptions import ReadTimeout, ConnectionError, RequestException
+import time
+from datetime import datetime
 
 file_path="/data/wang/9000_status.txt"
 #url="http://:120.132.50.181:9090/api/misc/db/test/334834"
 url="http://123.59.53.69:9000/api/misc/db/test/334834"
+t=datetime.now()
+print(t)
 
 try:
-    response = requests.get(url, timeout = 0.5)
-    print(response.status_code)
+    url_status = requests.get(url,timeout=0.002)
+    f = open(file_path,"a+")
+    f.write(str(t))
+    f.write('服务器第一次检测正常'+'\n')
+    f.close()
+    print(url_status.status_code)
 except ReadTimeout:
     print('Timeout')
+    f = open(file_path,"a+")
+    f.write(str(t)+'服务器第一次检测不正常'+'\n')
+    f.close()
 except ConnectionError:
     print('Connection error')
 except RequestException:
     print('Error')
-	
-	
 
-if response.status_code==200:
-    f = open(file_path,'a')
-    f.write('服务器第一次检测正常'+'\n')
-    f.close()
-    print(response.status_code)
-else:
-    f = open(file_path,'a')
-    f.write('服务器第一次检测不正常'+'\n')
-    f.close()
-    print(response.status_code)
