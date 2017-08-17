44cbc0a08ede177eb2cb0116189fc0b091a32dca
wangle <flywangle@hotmail.com>
2017-08-17 14:06:03 +0800

    改用相对变量取代绝对变量

--- monitor.py
+++ monitor.py
@@ -10,16 +10,16 @@ import commands
 import time
 from datetime import datetime
 
-def cws_restart():
-    status=commands.getstatusoutput('/data/wang/stop.sh')
+def cws_restart(path):
+    status=commands.getstatusoutput(path+'stop.sh')
     #print status
     time.sleep(1)
-    status=commands.getstatusoutput('/data/wang/startup.sh')
+    status=commands.getstatusoutput(path+'startup.sh')
     #print status
     return
 
-def log(t,context):
-    f = open(log_file,"a+")
+def log(file,context):
+    f = open(file,"a+")
     f.write(str(t)+context+'\n')
     f.close()
     return
@@ -30,44 +30,44 @@ def sendmail():
     commands.getstatusoutput(command_content)
     return
 
-def check_restart():
+def check_restart(log_file,check_file):
      f = open(check_file,"r")
      flag=f.read()
      f.close()
      if int(flag) == 0:
-         cws_restart()
+         cws_restart(path1)
          f2 = open(check_file,"w")
          f2.write('2')
          f2.close()
-         log(t,'服务器检测不正常，已重启')
+         log(log_file,'服务器检测不正常，已重启')
      else:
-         log(t,'服务器已经在1小时内重启过,无需再重启')
+         log(log_file,'服务器已经在1小时内重启过,无需再重启')
      return
 
 
-
-log_file="/data/wang/9000_status.txt"
-check_file="/data/wang/cws_9000_status.txt"
+path1="/data/wang/"    #监控程序所在目录
+log_file=path1+"9000_status.txt"
+check_file=path1+"cws_9000_status.txt"
 #url="http://:120.132.50.181:9090/api/misc/db/test/334834"
 url="http://123.59.53.69:9000/api/misc/db/test/334834"
 t=datetime.now()
 
 try:
     url_status = requests.get(url,timeout=0.002)
-    log(t,'服务器检测正常')
+    log(log_file,'服务器检测正常')
     print(url_status.status_code)
 
 except ReadTimeout:
     print('Timeout')
     if __name__=='__main__':
-        check_restart()
+        check_restart(log_file,check_file)
     sendmail()
  
 except ConnectionError:
     print('Connection error')
-    log(t,context='Connection error.服务器检测不正常')
+    log(log_file,context='Connection error.服务器检测不正常')
 
 except RequestException:
     print('Error')
-    log(t,context='Error.服务器检测不正常')
+    log(log_file,context='Error.服务器检测不正常')
 
