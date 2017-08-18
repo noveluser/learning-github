3083180480d64db29c302e2b010a59e19ceea67a
wangle <flywangle@hotmail.com>
2017-08-18 10:16:06 +0800

    增加多端口循环

--- restart_cws.py
+++ restart_cws.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/python2.7
+#!/usr/local/bin/python2.7      
 #coding=utf-8
 
 ' a restart scription when cws halt'
-__author__ = 'Alex Wang'
+__author__ = 'Alex Wang'             #标准文件模板
 
 import requests
 from requests.exceptions import ReadTimeout, ConnectionError, RequestException
@@ -11,12 +11,12 @@ import time
 from datetime import datetime
 
 def cws_restart(path):
-    status=commands.getstatusoutput(path+'stop_9000.sh')
+    status=commands.getstatusoutput(path+'stop_'+port+'.sh')
     #print status
     time.sleep(1)
-    status=commands.getstatusoutput(path+'startup_9000.sh')
+    status=commands.getstatusoutput(path+'startup_'+port+'.sh')
     #print status
-    return
+    return         #return是标准函数最后一句
 
 def log(file,context):
     f = open(file,"a+")
@@ -30,7 +30,7 @@ def sendmail():
     commands.getstatusoutput(command_content)
     return
 
-def check_restart(log_file,check_file):
+def check_restart(log_file,check_file,port):
      f = open(check_file,"r")
      flag=f.read()
      f.close()
@@ -43,34 +43,45 @@ def check_restart(log_file,check_file):
      else:
          log(log_file,'服务器已经在1小时内重启过,无需再重启')
      return
+	 
+def check_url(port):
+    log_file=path1+"cws_"+port+"_status.log"     #日志文件绝对路径
+    check_file=path1+port+"_status.txt"       #设置重启标志的文件，如果重启，那么完成后写入重启标志1
+    url="http://123.59.53.69:"+port+"/api/misc/db/test/334834"       #检测URL路径
+    cws_status = 0
+    try:
+        url_status = requests.get(url,timeout=1.002)
+        log(log_file,'服务器检测正常')
+        print(url_status.status_code)
+    except ReadTimeout:
+        print('Timeout')
+        cws_status = 1       #异常状态标志为1
+ 
+    except ConnectionError:
+        print('Connection error')
+        log(log_file,context='Connection error.服务器检测不正常')
 
+    except RequestException:
+        print('Error')
+        log(log_file,context='Error.服务器检测不正常')
+    if cws_status == 1:           #如果异常，那么进入重启模块
+        if __name__=='__main__':
+            check_restart(log_file,check_file,port)
+        print(t,cws_status,log_file,check_file)
+        sendmail()	
+    return 
 
+ports=['9000','9090']
 path1="/data/cyy928/crond/"    #监控程序所在目录
-log_file=path1+"cws_9000_status.log"
-check_file=path1+"9000_status.txt"
+
 #url="http://:120.132.50.181:9090/api/misc/db/test/334834"
-url="http://123.59.53.69:9000/api/misc/db/test/334834"
+#url="http://123.59.53.69:9000/api/misc/db/test/334834"       #检测URL路径
+
 t=datetime.now()
 
-try:
-    url_status = requests.get(url,timeout=1.002)
-    log(log_file,'服务器检测正常')
-    print(url_status.status_code)
-    cws_status = 0
-except ReadTimeout:
-    print('Timeout')
-    cws_status = 1
- 
-except ConnectionError:
-    print('Connection error')
-    log(log_file,context='Connection error.服务器检测不正常')
 
-except RequestException:
-    print('Error')
-    log(log_file,context='Error.服务器检测不正常')
+for port in ports:
+	if __name__=='__main__':    
+	    check_url(port)
 
-if cws_status == 1:
-    if __name__=='__main__':
-        check_restart(log_file,check_file)
-    print(cws_status)
-    sendmail()
+	
